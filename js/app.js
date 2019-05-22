96494b9d0946aa370c6bea5914867a962421ea7c
Lara Darine <delara.risal2017@gmail.com>
2019-05-22 23:16:00 +0100

    Add loading animation :racehorce:

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', ()=> {
+    let loader = document.querySelector('#loader');
+    loader.classList.add('loaded');
+    console.log('loaded', loader)
+});
+
+
+let hamburger = document.querySelector('#hamburger-menu');
+
+hamburger.addEventListener('click', () => {
+    console.log(hamburger.children[0]);
+    hamburger.classList.toggle('close')
+});
