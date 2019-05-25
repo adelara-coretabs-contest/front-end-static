4f2da1c26a4921f619760fc8be89f81a5ecbf2e9
Lara Darine <delara.risal2017@gmail.com>
2019-05-25 05:34:43 +0100

    Add order form :sparkles:

--- js/app.js
+++ js/app.js
@@ -18,3 +18,20 @@ let scrollToMeals = ()=> {
     downToMeals.scrollIntoView(meals);
     console.log('meals');
 }
+
+let toggleForm = (element)=> {
+    let form = document.querySelector('.form');
+    let ingredients = document.querySelector('#ingredients');
+    let mealName = document.querySelector('#meal-name');
+    let thisElement = document.querySelector(`#${element}`)
+    console.log(form)
+    form.classList.toggle('hidden');
+    ingredients.classList.toggle('hidden');
+    mealName.classList.toggle('hidden');
+    if (thisElement.textContent == 'Order now') {
+        thisElement.textContent = 'Cancel';
+    } else {
+        thisElement.textContent = 'Order now'
+    }
+    console.log(thisElement)
+}
